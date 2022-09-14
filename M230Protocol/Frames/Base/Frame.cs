f718e98a65baccf96e5db6e913e18df4409f7619
Kirill Novikov <GreezXII@protonmail.ch>
2022-09-14 20:52:45 +0300

    Add Request class

---
+++ M230Protocol/Frames/Base/Frame.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace M230Protocol.Frames.Base
+{
+    /* Base class for all frames.
+     * +---------+---------+
+     * | Address |   CRC   |
+     * | 1 byte  | 2 bytes |
+     * +---------+---------+
+     * 
+     * Address - address of meter.
+     * CRC - Cyclic redundancy check, checksum for data integrity. 
+     *       CRC16 with MODBUS polynomial
+     * 
+     * This is base class for other frames.
+     */
+    class Frame
+    {
+        public byte Address { get; protected set; }
+        public byte[] CRC { get; protected set; }
+        public int Length { get; protected set; }
+
+        public Frame() { }
+
+        public Frame(byte addr)
+        {
+            Address = addr;
+            Length = 3;
+        }
+
+        internal byte[] CalculateCRC16Modbus(byte[] buffer)
+        {
+            ushort crc = 0xFFFF;
+            for (int pos = 0; pos < buffer.Length; pos++)
+            {
+                crc ^= (ushort)buffer[pos];
+                for (int i = 8; i != 0; i--)
+                {
+                    if ((crc & 0x0001) != 0)
+                    {
+                        crc >>= 1;
+                        crc ^= 0xA001;
+                    }
+                    else
+                        crc >>= 1;
+                }
+            }
+            CRC = BitConverter.GetBytes(crc);
+            return CRC;
+        }
+
+        public static bool CRCMatch(byte[] a, byte[] b)
+        {
+            if (a == null || b == null)
+                return false;
+            if (a.Length != b.Length)
+                return false;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] != b[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ M230Protocol/Frames/Base/Request.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M230Protocol.Frames.Base
+{
+    /* Base class for all requests.
+     * +---------+------------------+---------+
+     * | Address |   Request type   |   CRC   |
+     * | 1 byte  |      1 byte      | 2 bytes |
+     * +---------+------------------+---------+
+     * 
+     * Address - address of meter.
+     * Request type - defines the functionality of the request.
+     * CRC - Cyclic redundancy check, checksum for data integrity. 
+     *       CRC16 with MODBUS polynomial.
+     * 
+     * This is base class for other requests.
+     */
+    abstract class Request : Frame
+    {
+        protected enum RequestTypes : byte
+        {
+            TestConnection = 0x00,  // Test link
+            OpenConnection = 0x01,  // Open connection
+            CloseConnection = 0x02, // Close connection
+            ReadSettings = 0x08,    // Read setings
+            ReadJournal = 0x04,     // Read journal
+            ReadArray = 0x05,       // Read energy arrays within 12 months
+            WriteSettings = 0x03,   // Write settings
+        }
+
+        public byte RequestType { get; protected set; }
+        public int ResponseLength { get; protected set; }  //TOD: What is response length? Write docs for response length
+
+        public Request(byte addr)
+            : base(addr)
+        {
+            Length += 1;
+            ResponseLength = 4;
+        }
+        public abstract byte[] Create();
+
+        internal byte[] StringToBCD(string s)  // BCD - Binary-coded decimal
+        {
+            byte[] bytePassword = new byte[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                byte b;
+                bool result = byte.TryParse(s[i].ToString(), out b);
+                if (!result)
+                    throw new Exception($"Не удалось преобразовать {s} в двоично-десятичное представление.");
+                bytePassword[i] = b;
+            }
+            return bytePassword;
+        }
+    }
+}
