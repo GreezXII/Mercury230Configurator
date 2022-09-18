76b137c44bcb4eaea6a63f250605d6954c4d5a2f
Kirill Novikov <GreezXII@protonmail.ch>
2022-09-18 18:24:25 +0300

    Create CommunicationStateResponse

---
+++ M230Protocol/Frames/Responses/CommunicationStateResponse.cs
@@ -0,0 +1,23 @@
+﻿using M230Protocol.Frames.Base;
+
+namespace M230Protocol.Frames.Responses
+{
+    internal class CommunicationStateResponse : Response
+    {
+        public enum CommunicationState : byte
+        {
+            OK,
+            WrongCommandOrParameter,
+            InternalMeterError,
+            AccessDenied,
+            LinkClosed
+        }
+
+        public CommunicationState State { get; private set; }
+        public CommunicationStateResponse(byte[] response) : base(response)
+        {
+            Length = 2;
+            State = (CommunicationState)response[1];
+        }
+    }
+}
