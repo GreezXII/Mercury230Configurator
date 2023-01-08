1c2d2e624919ced9505a6eab2fb96d29e07d760f
GreezXII <GreezXII@protonmail.ch>
2023-01-08 16:44:27 +0300

    Write documentation for MeterClient

--- MeterClient/SerialPortClient.cs
+++ MeterClient/SerialPortClient.cs
@@ -1,53 +1,66 @@
 ﻿using System.IO.Ports;
-using M230Protocol.Frames.Base;
-using System;
 
 namespace MeterClient
 {
-    public class SerialPortClient
+	/// <summary>
+	/// Implements transfer of byte arrays between client and meter.
+    /// </summary>
+	public class SerialPortClient
     {
         public SerialPort SerialPort { get; private set; }
-        public SerialPortClient(string portName, 
-                                int baudrate = 9600, 
-                                Parity parity = Parity.None, 
-                                int dataBits = 8, 
-                                StopBits portStopBits = StopBits.One, 
-                                int readTimeout = 5000)
+        public SerialPortClient(string portName, int timeout = 5000)
         {
             SerialPort = new SerialPort
             {
                 PortName = portName,
-                BaudRate = baudrate,
-                Parity = parity,
-                DataBits = dataBits,
-                StopBits = portStopBits,
-                ReadTimeout = readTimeout
-            };
+                BaudRate = 9600,
+                Parity = Parity.None,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                ReadTimeout = timeout,
+                WriteTimeout = timeout
+			};
         }
-
-        private async Task<byte[]> ReadFromSerialPortAsync(byte[] outputData, int count, CancellationToken token = default)
+		/// <summary>
+		/// Send a byte array to the meter and get a response byte array.
+		/// </summary>
+		/// <param name="outputData">Data that should be sent to the meter. To get the right byte array you should use Create() method for child type of <seealso cref="Request"/> type.</param>
+		/// <param name="inputDataLength">The length of expected response byte array.</param>
+		/// <param name="token">Propagates notification that operations should be canceled.</param>
+		/// <returns>A Task that returns a response byte array.</returns>
+		/// <exception cref="OperationCanceledException"></exception>
+		private async Task<byte[]> MakeDataExchange(byte[] outputData, int inputDataLength, CancellationToken token = default)
         {
             using (SerialPort)
             {
                 SerialPort.Open();
                 await SerialPort.BaseStream.WriteAsync(outputData, token);
 
-				byte[] inputData = new byte[count];
+				byte[] inputData = new byte[inputDataLength];
 				int offset = 0;
-				while (count > 0)
+				while (inputDataLength > 0)
                 {
                     if (token.IsCancellationRequested)
                         throw new OperationCanceledException();
-                    int bytesRead = await SerialPort.BaseStream.ReadAsync(inputData.AsMemory(offset, count), token);
+                    int bytesRead = await SerialPort.BaseStream.ReadAsync(inputData.AsMemory(offset, inputDataLength), token);
                     offset += bytesRead;
-                    count -= bytesRead;
+                    inputDataLength -= bytesRead;
                 }
                 return inputData;
             }
         }
-		public async Task<byte[]> GetResponseAsync(byte[] buffer, int count, CancellationToken token = default)
+		/// <summary>
+		/// Send a byte array to the meter and get a response byte array. This method extends <seealso cref="MakeDataExchange(byte[], int, CancellationToken)"/>
+		/// with timeout Task as <seealso cref="SerialPort.ReadTimeout"/> and <seealso cref="SerialPort.WriteTimeout"/> could work incorrectly with virtual serial ports.
+		/// </summary>
+		/// <param name="outputData">Data that should be sent to the meter. To get the right byte array you should use Create() method for child type of <seealso cref="Request"/> type.</param>
+		/// <param name="inputDataLength">The length of expected response byte array.</param>
+		/// <param name="token">Propagates notification that operations should be canceled.</param>
+		/// <returns>A Task that returns a response byte array.</returns>
+		/// <exception cref="TimeoutException"></exception>
+		public async Task<byte[]> GetResponseAsync(byte[] outputData, int inputDataLength, CancellationToken token = default)
         {
-            Task<byte[]> readFromSerialPortTask = ReadFromSerialPortAsync(buffer, count, token);
+            Task<byte[]> readFromSerialPortTask = MakeDataExchange(outputData, inputDataLength, token);
             var completedTask = await Task.WhenAny(readFromSerialPortTask, Task.Delay(SerialPort.ReadTimeout, token));
             if (completedTask == readFromSerialPortTask)
                 return await readFromSerialPortTask;
