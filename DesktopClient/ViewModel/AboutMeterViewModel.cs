c754e212ba26debf3da6fb50e12a44560bbc5699
Kirill Novikov <kirill.novikov.dev@gmail.com>
2023-03-29 20:17:08 +0300

    Remove custom base class for ViewModels

--- DesktopClient/ViewModel/AboutMeterViewModel.cs
+++ DesktopClient/ViewModel/AboutMeterViewModel.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DesktopClient.Base;
 
 namespace DesktopClient.ViewModel
 {
-    class AboutMeterViewModel : BaseViewModel
+    class AboutMeterViewModel
     {
     }
 }
