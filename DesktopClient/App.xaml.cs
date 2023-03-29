6c5b559049878ce2248a7bb5c644538ccf48a04b
Kirill Novikov <kirill.novikov.dev@gmail.com>
2023-03-29 22:47:08 +0300

    Implement navigation with MVVM lib

--- DesktopClient/App.xaml.cs
+++ DesktopClient/App.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using DesktopClient.ViewModel;
 
 namespace DesktopClient
 {
@@ -13,5 +10,26 @@ namespace DesktopClient
     /// </summary>
     public partial class App : Application
     {
+        public new static App Current => (App) Application.Current;
+        public IServiceProvider Services { get; }
+
+        public App()
+        {
+            Services = ConfigureServices();
+            this.InitializeComponent();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<ConnectionViewModel>();
+            services.AddSingleton<AboutMeterViewModel>();
+            services.AddSingleton<JournalsViewModel>();
+            services.AddSingleton<EnergyViewModel>();
+
+            return services.BuildServiceProvider();
+        }
     }
 }
--- DesktopClient/ViewModel/MainViewModel.cs
+++ DesktopClient/ViewModel/MainViewModel.cs
@@ -1,14 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Navigation;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DesktopClient.ViewModel
 {
-    class MainViewModel
+    partial class MainViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private ObservableObject? _connectionViewModel;
+        [ObservableProperty]
+        private ObservableObject? _aboutMeterViewModel;
+        [ObservableProperty]
+        private ObservableObject? _energyViewModel;
+        [ObservableProperty]
+        private ObservableObject? _journalsViewModel;
+        
+        [ObservableProperty]
+        public ObservableObject? _selectedViewModel;
 
+        public MainViewModel()
+        {
+            _connectionViewModel = App.Current.Services.GetService<ConnectionViewModel>();
+            _aboutMeterViewModel = App.Current.Services.GetService<AboutMeterViewModel>();
+            _journalsViewModel = App.Current.Services.GetService<JournalsViewModel>();
+            _energyViewModel = App.Current.Services.GetService<EnergyViewModel>();
+            SelectedViewModel = _connectionViewModel;
+        }
+
+        [RelayCommand]
+        private void ChangeSelectedViewModel(ObservableObject? viewModel) 
+            => SelectedViewModel = viewModel;
+
+        [RelayCommand]
+        private static void CloseApp() => App.Current.Shutdown();
     }
 }
