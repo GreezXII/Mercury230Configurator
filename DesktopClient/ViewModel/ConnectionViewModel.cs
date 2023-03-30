14f5c3bf463ee2b0d64d33d537746a20fac3c313
Kirill Novikov <kirill.novikov.dev@gmail.com>
2023-03-30 20:02:06 +0300

    Implement combobox for meter access level

--- DesktopClient/ViewModel/ConnectionViewModel.cs
+++ DesktopClient/ViewModel/ConnectionViewModel.cs
@@ -1,13 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using M230Protocol;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 
 namespace DesktopClient.ViewModel
 {
     class ConnectionViewModel : ObservableObject
     {
+        public string[] AccessLevelNames { get; }
+        MeterAccessLevels _selectedAccessLevel;
+        string? _selectedAccessLevelName;
+        public string? SelectedAccessLevelName
+        {
+            get => _selectedAccessLevelName;
+            set
+            {
+                SetProperty(ref _selectedAccessLevelName, value);
+                SetMeterAccessLevel(ref _selectedAccessLevel, SelectedAccessLevelName);
+            }
+        }
+
+        public ConnectionViewModel()
+        {
+            AccessLevelNames = new string[] { "Пользователь", "Администратор" };
+            SelectedAccessLevelName = AccessLevelNames[0];
+            _selectedAccessLevel = MeterAccessLevels.User;
+        }
+
+        private static void SetMeterAccessLevel(ref MeterAccessLevels field, string? level)
+        {
+            switch (level)
+            {
+                case "Администратор":
+                    field = MeterAccessLevels.Admin;
+                    break;
+                case "Пользователь":
+                    field = MeterAccessLevels.User;
+                    break;
+                default:
+                    throw new ArgumentException("Передан неизвестный уровень доступа.");
+            }
+        }
     }
 }
