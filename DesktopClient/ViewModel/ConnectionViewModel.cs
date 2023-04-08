638e2004ea155c7ac183129bf072c327ed0841fa
Kirill Novikov <kirill.novikov.dev@gmail.com>
2023-04-08 14:02:51 +0300

    Add timeout field to ConnectView

--- DesktopClient/ViewModel/ConnectionViewModel.cs
+++ DesktopClient/ViewModel/ConnectionViewModel.cs
@@ -17,13 +17,7 @@ namespace DesktopClient.ViewModel
 {
     partial class ConnectionViewModel : ObservableValidator
     {
-        [ObservableProperty]
-        private bool _IsError;
-
-        [ObservableProperty]
-        [NotifyDataErrorInfo]
-        [MinLength(1)]
-        [Digits]
+        [ObservableProperty, NotifyDataErrorInfo, MinLength(1), Required, Digits]
         private string? _address;
 
         public string[] AccessLevelNames { get; }
@@ -42,12 +36,16 @@ namespace DesktopClient.ViewModel
         public string[] SerialPortsNames { get; }
         public string? SelectedSerialPort { get; set; }
 
+        [ObservableProperty, NotifyDataErrorInfo, MinLength(2), Required, Digits]
+        private string _connectionTimeout;
+
         public Meter Meter { get; set; }
         public ProgressService ProgressService { get; }
         public MeterConnectionService MeterConnectionService { get; }
 
         public ConnectionViewModel()
         {
+            
             // Init access levels
             AccessLevelNames = new string[] { "Пользователь", "Администратор" };
             SelectedAccessLevelName = AccessLevelNames[0];
@@ -57,6 +55,9 @@ namespace DesktopClient.ViewModel
             SerialPortsNames = SerialPort.GetPortNames();
             if (SerialPortsNames.Length > 0)
                 SelectedSerialPort = SerialPortsNames[0];
+
+            // Init timeouts
+            ConnectionTimeout = "5000";
 
             // Init Meter
             Meter = App.Current.Services.GetService<Meter>() ?? throw new NullReferenceException("Не удалось создать экземпляр класса Meter.");
@@ -85,14 +86,9 @@ namespace DesktopClient.ViewModel
             ProgressService.IsTaskRunning = true;
             try
             {
-                if (Address == null || !byte.TryParse(Address, out byte address))
+                if (Address == null)
                 {
-                    MessageBox.Show("Неверно задан адрес счетчика.", "Ошибка", MessageBoxButton.OK);
-                    return;
-                }
-                if (passwordBox == null)
-                {
-                    MessageBox.Show("Пароль не может быть null.", "Ошибка", MessageBoxButton.OK);
+                    Address = string.Empty;
                     return;
                 }
                 if (SelectedSerialPort == null)
@@ -100,14 +96,17 @@ namespace DesktopClient.ViewModel
                     MessageBox.Show("Не выбран Com-порт.", "Ошибка", MessageBoxButton.OK);
                     return;
                 }
-                Meter = new Meter(address, SelectedSerialPort);
+                //Meter = new Meter(address, SelectedSerialPort);
+                Meter.Address = byte.Parse(Address);
+                Meter.SerialPort.PortName = SelectedSerialPort;
+                Meter.SerialPort.Timeout = int.Parse(ConnectionTimeout);
                 CommunicationState linkTest = await Meter.TestLinkAsync(token);
                 if (linkTest != CommunicationState.OK)
                 {
                     MessageBox.Show("Не удалось установить физическое соединение со счётчиком. Проверьте подключение.", "Ошибка", MessageBoxButton.OK);
                     return;
                 }
-                CommunicationState authorizationState = await Meter.OpenConnectionAsync(_selectedAccessLevel, passwordBox.SecurePassword, token);
+                CommunicationState authorizationState = await Meter.OpenConnectionAsync(_selectedAccessLevel, passwordBox!.SecurePassword, token);
                 if (authorizationState != CommunicationState.OK)
                 {
                     MessageBox.Show("Не удалось выполнить авторизацию. Проверьте правильность ввода пароля.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Exclamation);
--- MeterClient/SerialPortClient.cs
+++ MeterClient/SerialPortClient.cs
@@ -1,47 +1,54 @@
-﻿using System.IO.Ports;
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO.Ports;
 
 namespace MeterClient
 {
-	/// <summary>
-	/// Implements transfer of byte arrays between client and meter.
+    /// <summary>
+    /// Implements transfer of byte arrays between client and meter.
     /// </summary>
-	public class SerialPortClient
+    public class SerialPortClient
     {
-        public string PortName { get; set; }
+        public string PortName 
+        {
+            set
+            {
+                if (SerialPort.IsOpen)
+                    SerialPort.Close();
+                SerialPort.PortName = value;
+            }
+        }
         public int Timeout { get; set; }
-        public SerialPort? SerialPort { get; private set; }
+        private SerialPort SerialPort { get; set; }
+        
         public SerialPortClient() : this("COM99") { }
         public SerialPortClient(string portName, int timeout = 10000)
         {
-            PortName = portName;
-            Timeout = timeout;
             SerialPort = new SerialPort
             {
-                PortName = PortName,
                 BaudRate = 9600,
                 Parity = Parity.None,
                 DataBits = 8,
                 StopBits = StopBits.One,
-                ReadTimeout = Timeout,
-                WriteTimeout = Timeout
             };
+            PortName = portName;
+            Timeout = timeout;
         }
 
-		/// <summary>
-		/// Send a byte array to the meter and get a response byte array.
-		/// </summary>
-		/// <param name="outputData">Data that should be sent to the meter. To get the right byte array you should use Create() method for child type of <seealso cref="Request"/> type.</param>
-		/// <param name="inputDataLength">The length of expected response byte array.</param>
-		/// <param name="token">Propagates notification that operations should be canceled.</param>
-		/// <returns>A Task that returns a response byte array.</returns>
-		/// <exception cref="OperationCanceledException"></exception>
-		private async Task<byte[]> MakeDataExchange(byte[] outputData, int inputDataLength, CancellationToken token = default)
+        /// <summary>
+        /// Send a byte array to the meter and get a response byte array.
+        /// </summary>
+        /// <param name="outputData">Data that should be sent to the meter. To get the right byte array you should use Create() method for child type of <seealso cref="Request"/> type.</param>
+        /// <param name="inputDataLength">The length of expected response byte array.</param>
+        /// <param name="token">Propagates notification that operations should be canceled.</param>
+        /// <returns>A Task that returns a response byte array.</returns>
+        /// <exception cref="OperationCanceledException"></exception>
+        private async Task<byte[]> MakeDataExchange(byte[] outputData, int inputDataLength, CancellationToken token = default)
         {
-            SerialPort!.Open();
+            SerialPort.Open();
             await SerialPort.BaseStream.WriteAsync(outputData, token);
 
-		    byte[] inputData = new byte[inputDataLength];
-		    int offset = 0;
+            byte[] inputData = new byte[inputDataLength];
+            int offset = 0;
             while (inputDataLength > 0)
             {
                 int bytesRead = await SerialPort.BaseStream.ReadAsync(inputData.AsMemory(offset, inputDataLength), token);
@@ -50,21 +57,21 @@ namespace MeterClient
             }
             return inputData;
         }
-		/// <summary>
-		/// Send a byte array to the meter and get a response byte array. This method extends <seealso cref="MakeDataExchange(byte[], int, CancellationToken)"/>
-		/// with timeout Task as <seealso cref="SerialPort.ReadTimeout"/> and <seealso cref="SerialPort.WriteTimeout"/> could work incorrectly with virtual serial ports.
-		/// </summary>
-		/// <param name="outputData">Data that should be sent to the meter. To get the right byte array you should use Create() method for child type of <seealso cref="Request"/> type.</param>
-		/// <param name="inputDataLength">The length of expected response byte array.</param>
-		/// <param name="token">Propagates notification that operations should be canceled.</param>
-		/// <returns>A Task that returns a response byte array.</returns>
-		/// <exception cref="TimeoutException"></exception>
-		public async Task<byte[]> GetResponseAsync(byte[] outputData, int inputDataLength, CancellationToken token = default)
+        /// <summary>
+        /// Send a byte array to the meter and get a response byte array. This method extends <seealso cref="MakeDataExchange(byte[], int, CancellationToken)"/>
+        /// with timeout Task as <seealso cref="SerialPort.ReadTimeout"/> and <seealso cref="SerialPort.WriteTimeout"/> could work incorrectly with virtual serial ports.
+        /// </summary>
+        /// <param name="outputData">Data that should be sent to the meter. To get the right byte array you should use Create() method for child type of <seealso cref="Request"/> type.</param>
+        /// <param name="inputDataLength">The length of expected response byte array.</param>
+        /// <param name="token">Propagates notification that operations should be canceled.</param>
+        /// <returns>A Task that returns a response byte array.</returns>
+        /// <exception cref="TimeoutException"></exception>
+        public async Task<byte[]> GetResponseAsync(byte[] outputData, int inputDataLength, CancellationToken token = default)
         {
             using (SerialPort)
             {
                 Task<byte[]> readFromSerialPortTask = MakeDataExchange(outputData, inputDataLength, token);
-                Task timeoutTimer = Task.Delay(SerialPort!.ReadTimeout, token);
+                Task timeoutTimer = Task.Delay(Timeout, token);
                 var completedTask = await Task.WhenAny(readFromSerialPortTask, timeoutTimer);
                 if (completedTask.IsCanceled)
                     throw new OperationCanceledException();
